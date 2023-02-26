d14d7812686b3fc0aa826aff9d416634f1f00ec9
juaneins_uio <ballesterosjg@gmail.com>
2023-02-26 15:28:14 -0500

    show product detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { RegisterComponent } from './pages/register/register.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 const routes: Routes = [
   {
@@ -23,6 +24,10 @@ const routes: Routes = [
   {
     path: 'category/:id',
     component: CategoryComponent,
+  },
+  {
+    path: 'product/:id',
+    component: ProductDetailComponent,
   },
   {
     path: 'mycart',
