692a00d62e27c206d7837367feaad458abf4fed4
juaneins_uio <ballesterosjg@gmail.com>
2023-03-17 18:05:01 -0500

    create and moving to shared module

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SwiperModule } from 'swiper/angular';
 
-import { ImgComponent } from './components/img/img.component';
-import { ProductComponent } from './components/product/product.component';
-import { ProductsComponent } from './components/products/products.component';
+import { WebsiteRoutingModule } from './website-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
 import { NavComponent } from './components/nav/nav.component';
-import { ReversePipe } from './pipes/reverse.pipe';
-import { TimeAgoPipe } from './pipes/time-ago.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
 import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
@@ -20,17 +16,9 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 
-import { WebsiteRoutingModule } from './website-routing.module';
-
 @NgModule({
   declarations: [
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
     NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
     HomeComponent,
     CategoryComponent,
     MyCartComponent,
@@ -41,6 +29,6 @@ import { WebsiteRoutingModule } from './website-routing.module';
     ProductDetailComponent,
     LayoutComponent,
   ],
-  imports: [CommonModule, WebsiteRoutingModule, SwiperModule],
+  imports: [CommonModule, WebsiteRoutingModule, SwiperModule, SharedModule],
 })
 export class WebsiteModule {}
